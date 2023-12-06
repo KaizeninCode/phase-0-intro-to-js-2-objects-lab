8f580a14ed84ca0791d4de669a2ee548e5cc7c46
Phil Wekullo <philwekullo@gmail.com>
2023-12-06 12:30:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = 'Lil Uzi Vert'
+employee.streetAddress = ' 16 Kenyatta Road'
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  // create a new object that is a shallow copy of the employee
+  let newEmployee = { ...employee };
+  
+  // set the new value for the specified key
+  newEmployee[key] = value;
+  
+  // return the new object
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+  employee[key] = value
+  return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  // Clone the employee
+  let newEmployee = { ...employee };
+ 
+  // Delete the key from the new employee
+  delete newEmployee[key];
+ 
+  // Return the new employee
+  return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  // check if the key exists in the employee object
+  if (employee.hasOwnProperty(key)) {
+     // delete the key/value pair from the employee object
+     delete employee[key];
+  }
+ 
+  // return the modified employee object
+  return employee;
+ }
